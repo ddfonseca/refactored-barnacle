46769769bf0c86e2be913da09a76160acf6e9722
David Fonseca <olfdavid@gmail.com>
2025-02-08 12:42:18 -0300

    chore: disable logger in tests

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,11 +2,11 @@ import pino from 'pino';
 import { config } from '../config';
 
 export const logger = pino({
-  level: config.nodeEnv === 'development' ? 'debug' : 'info',
-  transport: {
+  level: process.env.NODE_ENV === 'test' ? 'silent' : config.nodeEnv === 'development' ? 'debug' : 'info',
+  transport: process.env.NODE_ENV !== 'test' ? {
     target: 'pino-pretty',
     options: {
       colorize: true
     }
-  }
+  } : undefined
 });
